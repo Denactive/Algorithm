338d317be5ca5343c38dbc329c77d10230b8594b
Денис <d.turchin@bk.ru>
2021-04-07 22:32:14 +0300

    4_3 done

--- dz-1/source files/4_3.cpp
+++ dz-1/source files/4_3.cpp
@@ -2,11 +2,13 @@
 
 void test_heap() {
 	std::cout << "heap test\n";
-	Comparator<int> idi_nahuy;
-	Heap<int> h(idi_nahuy);
+	Comparator<int> std_cmp;
+	Heap<int> h(std_cmp);
 	for (int i = 0; i < 15; i++)
 		h.insert(i);
 	h.print();
+
+	std::cout << "size: " << h.size() << std::endl;
 
 	std::cout << "peek top: " << h.peek_top() << std::endl;
 	h.print();
@@ -15,4 +17,59 @@ void test_heap() {
 		std::cout << "extract top: " << h.extract_top() << std::endl;
 		h.print();
 	}
+	std::cout << "size: " << h.size() << std::endl;
+}
+
+void run4_3(std::string s, std::string sa) {
+	std::stringstream ss;
+	ss << s;
+
+	int n = 0;
+	if (s.empty())
+		std::cin >> n;
+	else
+		ss >> n;
+
+	int cnt = 0;
+	Comparator<int> std_cmp;
+	Heap<int> h(std_cmp);
+	for (int i = 0; i < n; i++) {
+		int arival = 0;
+		int departure = 0;
+		if (s.empty())
+			std::cin >> arival >> departure;
+		else
+			ss >> arival >> departure;
+		h.insert(departure);
+		while (arival > h.peek_top())
+			h.extract_top();
+		if (cnt < h.size())
+			cnt = h.size();
+	}
+	std::cout << cnt << std::endl;
+	std::cout << "correct is: " << sa << std::endl;
+}
+
+void test4_3() {
+	int test_amount = 6;
+	std::string cases[] = {
+		"1\n10 20\n",
+		"2\n10 20\n20 25\n",
+		"3\n10 20\n20 25\n21 30\n",
+		"4\n10 20\n20 25\n21 30\n22 30\n",
+		"5\n10 20\n20 30\n21 30\n22 30\n30 31\n",
+		"5\n10 20\n20 25\n21 30\n22 30\n31 32\n",
+
+	};
+	std::string answers[] = {
+		"1\n",
+		"2\n",
+		"2\n",
+		"3\n",
+		"4\n",
+		"3\n",
+
+	};
+	for (int i = 0; i < test_amount; i++)
+		run4_3(cases[i], answers[i]);
 }
--- dz-1/source files/algorithms.cpp
+++ dz-1/source files/algorithms.cpp
@@ -3,9 +3,13 @@
 int main()
 {
     setlocale(LC_ALL, "RUS");
-    std::cout << "Задача 2.1\n";
+    //std::cout << "Задача 2.1\n";
     //run2_1();
     //test2_1();
-    test_heap();
+    
+    //run4_3();
+    std::cout << "Задача 4.3\n";
+    test4_3();
+    //test_heap();
     return 0;
 }
